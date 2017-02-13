bb3fd392825921e71a15baf4e10763dc1a58ecdf
Steve Perkins <steve@steveperkins.com>
2017-02-13 18:26:29 -0500

    Adds Vault functionality and fleshed-out documentation

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
 config-client-lib
 ================
+This is one of three Git repositories, which work together to demonstrate using [Consul](https://www.consul.io) and 
+[Vault](https://www.vaultproject.io) for configuration management:
+
+* https://github.com/steve-perkins/config-properties - Contains:
+  * Properties files for every environment (e.g. "dev", "staging", "production") in a hypothetical enterprise, and 
+    every application within each environment.
+  * A processor script, run by Gradle, which syncs all of the property information with Consul every time the Git 
+    repository is updated.
+* https://github.com/steve-perkins/config-client-lib - A client library which loads the appropriate config properties 
+  for a given environment and application from Consul and Vault.
+* https://github.com/steve-perkins/config-sample-app - A sample web application which retrieves its config properties 
+  using the client library, and displays them in the browser.
+  
+This demo shows the use case of having two types of config properties:
+
+1. **non-secret** values, which can and should be maintainable by developer teams (e.g. JDBC URL's).
+2. **secret** values, which should only be viewable or maintainable by people with specialized access (e.g. 
+   usernames and passwords)
+   
+The non-secret values are stored as-is in the `config-properties` repo, and loaded directly into Consul.  For *secret* 
+values, Git (and Consul) store a Vault path for that property.  When the `config-client-lib` library encounters a 
+secret, it loads the "true" value from this Vault path... and applications such as `config-sample-app` are none the 
+wiser.
+
+Setup
+=====
+1. Perform all of the steps outlines in the [config-properties](https://github.com/steve-perkins/config-properties) 
+   project's README file.
+2. Build this client lib, so that it will available in your local Maven dependency cache:
+
+```
+gradlew publishToMavenLocal
+```
+
+3. See [config-sample-app](https://github.com/steve-perkins/config-sample-app) project README for next steps.
--- src/main/java/com/steveperkins/config/PropertiesClient.java
+++ src/main/java/com/steveperkins/config/PropertiesClient.java
@@ -1,13 +1,16 @@
 package com.steveperkins.config;
 
+import com.bettercloud.vault.Vault;
+import com.bettercloud.vault.VaultConfig;
+import com.bettercloud.vault.VaultException;
+import com.bettercloud.vault.response.AuthResponse;
+import com.bettercloud.vault.response.LogicalResponse;
 import com.ecwid.consul.v1.ConsulClient;
 import com.ecwid.consul.v1.kv.model.GetValue;
 
-import java.util.AbstractMap;
-import java.util.List;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 /**
  * TODO: Document
@@ -17,39 +20,68 @@ public class PropertiesClient {
     /**
      * TODO: Document
      */
-    public Map<String, String> loadProperties() {
-        final String environment = Optional.ofNullable(System.getenv("ENVIRONMENT")).orElse("local");
-        final String application = Optional.ofNullable(System.getenv("APPLICATION"))
-                .orElseThrow(() -> new RuntimeException("The environment variable \"APPLICATION\" is not set"));
-        final String consulHost = Optional.ofNullable(System.getenv("CONSUL_HOST"))
-                .orElseThrow(() -> new RuntimeException("The environment variable \"CONSUL_HOST\" is not set"));
-        final String vaultUrl = Optional.ofNullable(System.getenv("VAULT_URL"))
-                .orElseThrow(() -> new RuntimeException("The environment variable \"VAULT_URL\" is not set"));
-        final String vaultUsername = Optional.ofNullable(System.getenv("VAULT_USERNAME"))
-                .orElseThrow(() -> new RuntimeException("The environment variable \"VAULT_USERNAME\" is not set"));
-        final String vaultPassword = Optional.ofNullable(System.getenv("VAULT_PASSWORD"))
-                .orElseThrow(() -> new RuntimeException("The environment variable \"VAULT_PASSWORD\" is not set"));
+    public static Map<String, String> loadProperties() {
 
+        // Validate system properties
+        final String environment = Optional.ofNullable(System.getProperty("ENVIRONMENT")).orElse("local");
+        final String application = Optional.ofNullable(System.getProperty("APPLICATION"))
+                .orElseThrow(() -> new RuntimeException("The system property \"APPLICATION\" is not set"));
+        final String consulHost = Optional.ofNullable(System.getProperty("CONSUL_HOST"))
+                .orElseThrow(() -> new RuntimeException("The system property \"CONSUL_HOST\" is not set"));
+        final String vaultUrl = Optional.ofNullable(System.getProperty("VAULT_URL"))
+                .orElseThrow(() -> new RuntimeException("The system property \"VAULT_URL\" is not set"));
+        final String vaultUsername = Optional.ofNullable(System.getProperty("VAULT_USERNAME"))
+                .orElseThrow(() -> new RuntimeException("The system property \"VAULT_USERNAME\" is not set"));
+        final String vaultPassword = Optional.ofNullable(System.getProperty("VAULT_PASSWORD"))
+                .orElseThrow(() -> new RuntimeException("The system property \"VAULT_PASSWORD\" is not set"));
+
+        // TODO
         if (environment.equals("local")) {
             return fromLocal();
         }
 
-        final ConsulClient consulClient = new ConsulClient(consulHost);
-        final String consulPath = String.format("%s/%s", environment, application);
-        final List<GetValue> properties = consulClient.getKVValues(consulPath).getValue();
-        return properties.stream()
-                .map(prop -> {
-                    final String key = prop.getKey().substring(prop.getKey().lastIndexOf('/') + 1);
-                    String value = prop.getValue();
-                    if (value.startsWith("__vault__")) {
-                        // TODO:  Read from Vault with path value.substring(9)
-                    }
-                    return new AbstractMap.SimpleEntry<>(key, value);
-                }).collect(Collectors.toMap(AbstractMap.SimpleEntry::getKey, AbstractMap.SimpleEntry::getValue));
+        try {
+            // Connect with Consul and Vault
+            final ConsulClient consul = new ConsulClient(consulHost);
+            final Vault vault = vaultConnection(vaultUrl, vaultUsername, vaultPassword);
+
+            // Iterate over all properties stored by Consul in this environment's and application's path
+            final String consulPath = String.format("%s/%s", environment, application);
+            final Map<String, String> properties = new HashMap<>();
+            for (final GetValue property : consul.getKVValues(consulPath).getValue()) {
+
+                // Load each key-value property from Consul...
+                final String key = property.getKey().substring(property.getKey().lastIndexOf('/') + 1);
+                String value = property.getDecodedValue();
+
+                // ... and if the value references a Vault path, then read its true value from Vault.
+                if (value.startsWith("__vault__")) {
+                    final String vaultPath = value.substring(9);
+                    final LogicalResponse logicalResponse = vault.logical().read(vaultPath);
+                    value = logicalResponse.getData().get(key);
+                }
+
+                properties.put(key, value);
+            }
+            return properties;
+        } catch (VaultException e) {
+            throw new RuntimeException(e);
+        }
     }
 
-    private Map<String, String> fromLocal() {
+    private static Map<String, String> fromLocal() {
         return null;
+    }
+
+    private static Vault vaultConnection(final String vaultUrl, final String vaultUsername, final String vaultPassword)
+            throws VaultException {
+        final VaultConfig authVaultConfig = new VaultConfig(vaultUrl);
+        final Vault authVault = new Vault(authVaultConfig);
+        final AuthResponse authResponse = authVault.auth().loginByUserPass(vaultUsername, vaultPassword);
+        final String token = authResponse.getAuthClientToken();
+
+        final VaultConfig vaultConfig = new VaultConfig(vaultUrl, token);
+        return new Vault(vaultConfig);
     }
 
 }
